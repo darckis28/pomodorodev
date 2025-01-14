be8a28643f138d03c0eda069ea8301961fc01563
andi aguirre <darckisgamer@gmail.com>
2025-01-13 19:27:20 -0500

    add: toggle functionality for settings button in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import SettingsIcon from "../icons/SettingIcon";
 
 const Header = () => {
+  const [show, setShow] = useState(false);
   return (
     <div className=" w-full py-4 px-5 flex justify-between items-center ">
       <h1 className="font-extrabold text-2xl ">Pomodore.Dev</h1>
       <div>
-        <button className="p-2 nm-concave-white  rounded-lg flex justify-center items-center gap-2 font-semibold">
+        <button
+          onClick={() => setShow(!show)}
+          className={`p-2 ${
+            show ? "nm-inset-slate-200" : "nm-concave-white"
+          }   rounded-lg flex justify-center items-center gap-2 font-semibold transition-all`}
+        >
           <SettingsIcon />
           <span>Personalizar</span>
         </button>
