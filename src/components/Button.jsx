1fbee7b3154fd4fded77c4038e4a55c1be54f7fd
andi aguirre <darckisgamer@gmail.com>
2025-01-13 19:27:40 -0500

    add: enable onClick functionality for Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,9 @@
-const Button = () => {
+const Button = ({ onclick }) => {
   return (
-    <button className="nm-concave-blue-400-xs rounded-lg p-3 text-white text-2xl font-bold">
+    <button
+      onClick={onclick}
+      className="nm-concave-blue-400-xs rounded-lg p-3 text-white text-2xl font-bold"
+    >
       Comenzar
     </button>
   );
