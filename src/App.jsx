63a2008ead0c70f742a55e3404ffe2624585dff2
andi aguirre <darckisgamer@gmail.com>
2025-01-14 10:26:35 -0500

    add: envolver componente App con ContextGob para gestión de estado global

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import Header from "./components/Header";
 import Pomodoro from "./components/Pomodoro";
 import { TaskSection } from "./components/TaskSection";
+import ContextGob from "./context/ContextGob";
 
 const App = () => {
   return (
-    <div className=" container mx-auto rounded-lg nm-inset-slate-200 overflow-hidden w-full ">
-      <Header />
-      <div className=" flex flex-col md:flex-row gap-4 w-full ">
-        <Pomodoro />
-        <TaskSection />
+    <ContextGob>
+      <div className=" container mx-auto rounded-lg nm-inset-slate-200 overflow-hidden w-full ">
+        <Header />
+        <div className=" flex flex-col md:flex-row gap-4 w-full ">
+          <Pomodoro />
+          <TaskSection />
+        </div>
       </div>
-    </div>
+    </ContextGob>
   );
 };
 export default App;
