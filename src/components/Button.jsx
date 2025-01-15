5ad088841616e1e1c65b0f1e25e438ec8722eafb
andi aguirre <darckisgamer@gmail.com>
2025-01-15 18:19:56 -0500

    add: modificar componentes Button y Circle para aceptar propiedades de color y estado

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
-const Button = ({ onclick }) => {
+const Button = ({ onclick, color, children }) => {
   return (
     <button
       onClick={onclick}
-      className="nm-concave-blue-400-xs rounded-lg p-3 text-white text-2xl font-bold"
+      className={`${color} rounded-lg p-3 text-white text-2xl font-bold`}
     >
-      Comenzar
+      {children}
     </button>
   );
 };
--- src/components/Circle.jsx
+++ src/components/Circle.jsx
@@ -1,15 +1,16 @@
 import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
-const Circle = ({ text, porcent }) => {
+const Circle = ({ text, porcent, state }) => {
   return (
     <div className="nm-concave-white-sm rounded-full p-2">
       <CircularProgressbar
         value={porcent}
         text={text}
         styles={buildStyles({
-          pathColor: "#60A5FA",
-          textColor: "#60A5FA",
+          pathColor: `${state ? "#60A5FA" : "#25f08a"}`,
+          textColor: `${state ? "#60A5FA" : "#25f08a"}`,
         })}
+        strokeWidth={3}
       />
     </div>
   );
