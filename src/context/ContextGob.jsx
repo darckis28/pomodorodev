0c37a2cd1aeee578d506fb40506327ca96750934
andi aguirre <darckisgamer@gmail.com>
2025-01-15 20:47:20 -0500

    add: implementar gestión de tareas utilizando useReducer en ContextGob

--- src/context/ContextGob.jsx
+++ src/context/ContextGob.jsx
@@ -1,8 +1,25 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer, useState } from "react";
 
 export const context = createContext(null);
 
+const reducer = (state, action) => {
+  if (action.type === "ADD") {
+    return [...state, action.payload];
+  }
+  if (action.type === "DELETE") {
+    return state.filter((task) => task.id !== action.payload.id);
+  }
+  if (action.type === "COMPLETED") {
+    return state.map((task) =>
+      task.id === action.payload.id
+        ? { ...task, completed: !task.completed }
+        : task
+    );
+  }
+};
+
 const ContextGob = ({ children }) => {
+  const [tasks, dispatchTasks] = useReducer(reducer, []);
   const [select, setSelect] = useState({
     name: "novice",
     selected: true,
@@ -10,7 +27,7 @@ const ContextGob = ({ children }) => {
     pomodoro: 10,
   });
   return (
-    <context.Provider value={{ select, setSelect }}>
+    <context.Provider value={{ select, setSelect, tasks, dispatchTasks }}>
       {children}
     </context.Provider>
   );
