1d7d96edee92043285a23ee4b34a1b16c4b571db
andi aguirre <darckisgamer@gmail.com>
2025-01-15 20:47:13 -0500

    add: integrar gestión de tareas utilizando ContextGob en los componentes Task y TaskSection

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,18 +1,29 @@
+import { useContgob } from "../hooks/useContgob";
 import DeleteIcon from "../icons/DeleteIcon";
 
-const Task = () => {
+const Task = ({ title, id, completed }) => {
+  const { dispatchTasks } = useContgob();
+
+  const compledTask = () => {
+    dispatchTasks({ type: "COMPLETED", payload: { id } });
+  };
   return (
     <li className="nm-concave-slate-200 py-2 px-3 flex justify-between items-center rounded-lg">
       <div className="flex gap-3">
         <input
           type="checkbox"
-          name=""
-          id=""
+          onChange={compledTask}
+          checked={completed}
         />
-        <span>oyaaa</span>
+        <span className={completed && "line-through text-black/60"}>
+          {title}
+        </span>
       </div>
       <div>
-        <button className="text-slate-400 hover:text-red-400 ">
+        <button
+          onClick={() => dispatchTasks({ type: "DELETE", payload: { id } })}
+          className="text-slate-400 hover:text-red-400 "
+        >
           <DeleteIcon />
         </button>
       </div>
--- src/components/TaskSection.jsx
+++ src/components/TaskSection.jsx
@@ -1,20 +1,41 @@
 import { useRef, useState } from "react";
 import PlusIcon from "../icons/PlusIcon";
 import Task from "./Task";
+import { useContgob } from "../hooks/useContgob";
 
 export const TaskSection = () => {
   const [add, setAdd] = useState(false);
   const input = useRef(null);
+  const { tasks, dispatchTasks } = useContgob();
 
-  function cancel() {
+  function resetInput() {
     input.current.value = "";
     setAdd(false);
+  }
+  function save() {
+    if (input.current.value === "") return;
+    dispatchTasks({
+      type: "ADD",
+      payload: {
+        description: input.current.value,
+        id: crypto.randomUUID(),
+        completed: false,
+      },
+    });
+    resetInput();
   }
   return (
     <div className=" md:min-w-80 nm-inset-slate-200 rounded-lg p-5">
       <h2 className="text-2xl font-bold mb-2">Tasks</h2>
-      <ul className="mb-2">
-        <Task />
+      <ul className="mb-2 flex flex-col gap-2">
+        {tasks.map((task) => (
+          <Task
+            key={task.id}
+            id={task.id}
+            title={task.description}
+            completed={task.completed}
+          />
+        ))}
       </ul>
 
       {add ? (
@@ -26,11 +47,14 @@ export const TaskSection = () => {
             className="bg-transparent w-full outline-none mb-2"
           />
           <div>
-            <button className="py-1 px-4 bg-blue-400 hover:bg-blue-500 text-white mr-3 rounded-lg">
+            <button
+              onClick={save}
+              className="py-1 px-4 nm-concave-blue-400 hover:bg-blue-500 text-white mr-3 rounded-lg"
+            >
               Save
             </button>
             <button
-              onClick={cancel}
+              onClick={resetInput}
               className="py-1 px-4 hover:text-red-400"
             >
               Cancel
